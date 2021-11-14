537fb99c79160817f23e9f743b43cdb314869ae0
Samas1503 <73703204+Samas1503@users.noreply.github.com>
2021-11-14 01:29:40 -0300

    Update Empresa.java

--- tp10-final/src/main/java/ar/edu/unju/escmi/poo/dominio/Empresa.java
+++ tp10-final/src/main/java/ar/edu/unju/escmi/poo/dominio/Empresa.java
@@ -3,6 +3,16 @@ package ar.edu.unju.escmi.poo.dominio;
 public class Empresa extends Cliente {
 	private int cuit;
 
+	//METODOS CONSTRUCTORES
+	
+	public Empresa(String nombre, String email, int telefono, int cuit) {
+		super(nombre, email, telefono);
+		this.cuit = cuit;
+	}
+
+	public Empresa() {
+	}
+
 	//METODOS ACCESORES
 	public int getCuit() {
 		return cuit;
@@ -12,23 +22,10 @@ public class Empresa extends Cliente {
 		this.cuit = cuit;
 	}
 
-	//METODOS CONSTRUCTORES
-	
-	public Empresa(String nombre, String email, int telefono, int cuit) {
-		super(nombre, email, telefono);
-		this.cuit = cuit;
-	}
-
-	public Empresa(String nombre, String email, int telefono) {
-		super(nombre, email, telefono);
-	}
-
 	@Override
 	public String toString() {
 		return "Empresa [cuit=" + cuit + ", getNombre()=" + super.getNombre() + ", getEmail()=" + super.getEmail()
 				+ ", getTelefono()=" + super.getTelefono() + "]";
 	}
 
-	
-	
 }
